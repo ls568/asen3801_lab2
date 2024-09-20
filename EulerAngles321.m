be006a9d34c3541875a0b9a8eb41b74dc48969c2
ls568 <157405976+ls568@users.noreply.github.com>
2024-09-20 13:18:59 -0600

    321update

--- EulerAngles321.m
+++ EulerAngles321.m
@@ -5,6 +5,17 @@ function attitude321 = EulerAngles321(DCM)
 % Inputs:
 % DCM: a rotation matrix
 
+R11 = DCM(1,1);
+R12 = DCM(1,2);
+R23 = DCM(2,3);
+R33 = DCM(3,3);
+
+alpha = atan(R23/R33);
+beta = -asin(R13);
+gamma = atan(R12/R11);
+
+attitude321 = [alpha, beta, gamma];
+
 % Outputs:
 % attitude321: 3 x 1 vector with the 3-2-1 Euler angles in the form 
 % attitude321 = [alpha, beta, gamma]' (In ASEN 3728 notation, this would be
